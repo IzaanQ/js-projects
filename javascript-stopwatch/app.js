4c305a8850e9d7d2de5c429181a9962aa21a0c8c
IzaanQ <77301724+IzaanQ@users.noreply.github.com>
2022-02-15 21:14:27 -0500

    Update app.js

--- javascript-stopwatch/app.js
+++ javascript-stopwatch/app.js
@@ -1,1 +1,32 @@
-js things
+window.onload = function(){
+
+    let seconds = 00;
+    let tens = 00;
+    let appendTens = document.getElementById("tens")
+    let appendSeconds = document.getElementById("second")
+    let buttonStart = document.getElementById('button-start');
+    let buttonStop = document.getElementById('button-stop');
+    let buttonReset = document.getElementById('button-reset');
+    let Interval ;
+
+    buttonStart.onclick = function() {
+
+        clearInterval(Interval);
+            Interval = setInterval(startTimer, 10);
+    }
+
+    buttonStop.onclick = function() {
+        clearInterval(Interval);
+    }
+
+    buttonReset.onclick = function() {
+        clearInterval(Interval);
+        tens = "00";
+        seconds = "00";
+        appendTens.innerHTML = tens;
+        appendSeconds.innerHTML = seconds;
+    }
+
+    function
+
+}
