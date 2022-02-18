a3f56eb616cb62206c38f9d263b2be67164d8a3f
Izaan Qaiser <77301724+IzaanQ@users.noreply.github.com>
2022-02-18 11:51:16 -0500

    Update app.js

--- javascript-clock/app.js
+++ javascript-clock/app.js
@@ -1,1 +1,29 @@
-js tings
+function showTime(){
+    let date = new Date();
+    let h = date.getHours();
+    let m = date.getMinutes();
+    let s = date.getSeconds();
+    let session = "am";
+    
+    if (h == 0) {
+        h = 12;
+    }
+
+    if (h > 12) {
+        h = h - 12;
+        session = "pm";
+    }
+
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+
+    let time = h + ":" + m + ":" + s + " " + session;
+
+    document.getElementById("MyClockDisplay").innerText = time;
+    document.getElementById("MyClockDisplay").innerContent = time;
+
+    setTimeout(showTime, 1000);
+}
+
+showTime();
